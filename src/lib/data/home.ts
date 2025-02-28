04968c042003315a1ba7b2e38d12df6b1ebed3dd
tlin41390 <linterence4@gmail.com>
2025-02-28 01:23:59 -0800

    made small edits to the home page

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -3,15 +3,15 @@ import { getSkills } from './skills';
 
 export const title = 'Home';
 
-export const name = 'Name';
+export const name = 'Terence';
 
-export const lastName = 'LastName';
+export const lastName = 'Lin';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'I Love Code, Sports, and Data';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ platform: Platform.GitHub, link: 'https://github.com/tlin41390' },
 	{
 		platform: Platform.Linkedin,
 		link: 'https://www.linkedin.com/'
@@ -26,7 +26,7 @@ export const links: Array<{ platform: Platform; link: string }> = [
 	},
 	{
 		platform: Platform.Email,
-		link: 'riadh_adrani@hotmail.fr'
+		link: 'linterences@gmail.com'
 	},
 	{
 		platform: Platform.Youtube,
@@ -38,4 +38,4 @@ export const links: Array<{ platform: Platform; link: string }> = [
 	}
 ];
 
-export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts');
+export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts','python','java','c','sql','azure','aws');
